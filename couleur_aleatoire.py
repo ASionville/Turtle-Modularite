1e79a5f090ddbc73d17a941d3626233b7c76a4d3
Aubin Sionville <57773476+ASionville@users.noreply.github.com>
2020-10-12 19:05:37 +0200

    Couleur fait

--- couleur_aleatoire.py
+++ couleur_aleatoire.py
@@ -1,4 +1,3 @@
-import turtle
 from random import randint
 
 def couleur_aleatoire():
@@ -6,4 +5,4 @@ def couleur_aleatoire():
     renvoie un triplet de 3 nombres entier compris entre 0 et 255
     Ce triplet correspond à une couleur codée en RVB
     '''
-    pass
+    return tuple(randint(0, 255) for _ in range(3))
