5de8d2fb95977a5a0b2d2dbfb412d379ba140dfb
Aubin Sionville <57773476+ASionville@users.noreply.github.com>
2020-10-13 08:22:41 +0200

    Rectangle et Trait fait

--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,5 @@
 import turtle
+from trait import trait
 
 def rectangle(x,y,w,h):
     '''
@@ -9,9 +10,14 @@ def rectangle(x,y,w,h):
     Cette fonction dessine un rectangle Le point de coordonnées (x,y) est
     sur le côté en bas au milieu
     '''
-
-    pass
-
+    turtle.penup() # On lève le crayon
+    turtle.setposition(x, y) # On position le crayon
+    turtle.pendown() # On baisse le crayon
+    trait(x, y, x + w, y)
+    trait(x + w, y, x + w, y + h)
+    trait(x, y, x, y + h)
+    trait(x, y + h, x + w, y + h)
+    turtle.penup()
 
 if __name__ == '__main__':
     rectangle(0,0,150,100)
--- trait.py
+++ trait.py
@@ -10,7 +10,7 @@ def trait(x1, y1, x2, y2):
     turtle.penup() # On lève le crayon 
     
 if __name__ == '__main__':
-    trait(0,0,100,150)
+    trait(0,0,100,100)
     # On ferme la fenêtre s'il y a un clique gauche
     turtle.exitonclick()
     
